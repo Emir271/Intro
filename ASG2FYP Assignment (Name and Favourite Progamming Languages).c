17088e0f266d1e9267cff7996299e7f0e226ca96
Emir271 <rasyadsoehardjo@gmail.com>
2025-09-03 15:38:01 +0700

    Add files via upload

---
+++ ASG2FYP Assignment (Name and Favourite Progamming Languages).c
@@ -0,0 +1,17 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    printf("Hi, my name is Emir! \n");
+    printf("Two of my favourite programming languages are C# and C++! \n");
+
+    return 0;
+}
